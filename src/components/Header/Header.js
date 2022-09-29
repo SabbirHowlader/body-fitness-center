8039a094e09fdd729f7eef5d0408bf8982ac4487
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-09-29 22:11:20 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+           <h1>BODY FITNESS CENTER</h1>
+           <div>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+            <a href="/shop">Shop</a>
+            <a href="contact">Contact</a>
+           </div>
+        </nav>
+    );
+};
+
+export default Header;
