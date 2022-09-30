2a4eda386d1105e75f5c896041c3ab2d53fc5c06
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-09-30 20:49:42 +0600

    added question

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div>
        <Header></Header>
        <Cart></Cart>
+       <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div className=''>
+            <h5>1. How dose react work?</h5>
+            <p>React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike</p>
+            <br />
+            <h5>2. What is the different props and state?</h5>
+            <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components</p>
+            <br />
+            <h5>3. What is work off use effect?</h5>
+            <p>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers. useEffect accepts two arguments. The second argument is optional.</p>
+        </div>
+    );
+};
+
+export default Question;
