ba1534b3d2fcf2e49521bbcf808dc4a78d384fa4
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-09-30 12:56:48 +0600

    added person

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,10 +19,25 @@ const Cart = () => {
                 products.map(product=><Product key={product.id} product={product}>
                 </Product>)
                }
-
             </div>
             <div className="cart-container">
-               <h1>this is card</h1>###
+               <div className='person'>
+                <h2>MD: Sabbir Howlader</h2>
+                <p>Dhaka, Bangladesh</p>
+               </div>
+               <div className='weight'>
+                <p>75kg <br /> Weight</p>
+                <p>6.3 <br /> Height</p>
+                <p>25yrs <br /> Age</p>
+               </div>
+               <h4>Add a breack</h4>
+               <div className='second'>
+                 <a href="">10s</a>
+                 <a href="">20s</a>
+                 <a href="">30s</a>
+                 <a href="">40s</a>
+                 <a href="">50s</a>
+               </div>
             </div>
         </div>
     );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,17 +3,19 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props.product);
-    const {name, img, time, price,age,} = props.product;
+    const {name, img, time, price, age} = props.product;
     return (
         <div className='product'>
             <img src={img} alt=""></img>
             <div className='product-info'>
                 <p className='product-name'>{name}</p>
-                <p>Price:{price}$</p>
-                <p><small>Age:{age}</small></p>
-                <p><small>Time:{time}s</small></p>
+                <p>Price: {price}$</p>
+                <p><small>Age: {age}</small></p>
+                <p><small>Time: {time}s</small></p>
             </div>
-            
+            <button className='btn-cart'>
+                <p>Add to list</p>
+            </button>
         </div>
     );
 };
