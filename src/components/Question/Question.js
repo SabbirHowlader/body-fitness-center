3713293e0a587d17b1f6a8d7b9546ff6a5fe425a
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-09-30 21:00:19 +0600

    added style

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Question.css'
 
 const Question = () => {
     return (
-        <div className=''>
+        <div className='question'>
             <h5>1. How dose react work?</h5>
             <p>React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike</p>
             <br />
